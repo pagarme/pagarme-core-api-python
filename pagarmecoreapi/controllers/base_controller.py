c7b11ca78ab3e7e896e5b75048e6f72b511db00e
Jeferson <jeferson.augusto@pagar.me>
2022-01-14 13:48:34 -0300

    5.5.0 - Created rule id in CreateSplitRequest. (#15)
    Co-authored-by: Unknown <samuel.leite@mundipagg.com>

--- pagarmecoreapi/controllers/base_controller.py
+++ pagarmecoreapi/controllers/base_controller.py
@@ -32,7 +32,7 @@ class BaseController(object):
     http_call_back = None
 
     global_headers = {
-        'user-agent': 'PagarmeCoreApi - Python 5.4.0'
+        'user-agent': 'PagarmeCoreApi - Python 5.5.0'
     }
 
     def __init__(self, client=None, call_back=None):
