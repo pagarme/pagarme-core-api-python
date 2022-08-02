036ad731cf3e7fcc9bcb7dffa057da4d2b33ec52
Magno <magno.moreira@pagar.me>
2022-08-02 17:22:38 -0300

    update version (#21)
    * update version
    
    * Update setup.py

--- pagarmecoreapi/controllers/base_controller.py
+++ pagarmecoreapi/controllers/base_controller.py
@@ -32,7 +32,7 @@ class BaseController(object):
     http_call_back = None
 
     global_headers = {
-        'user-agent': 'PagarmeCoreApi - Python 5.6.0'
+        'user-agent': 'PagarmeCoreApi - Python 5.6.1'
     }
 
     def __init__(self, client=None, call_back=None):
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 
 setup(
     name='pagarmecoreapi',
-    version='5.6.0',
+    version='5.6.1',
     description='Pagarme API',
     long_description=long_description,
     long_description_content_type="text/markdown",
